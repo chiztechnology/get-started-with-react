a38e177b27c4cc5fcb6ed5639d4f1d35bc1db3a7
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-01-18 23:54:33 +0200

    Design calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ButtonPanel from "./ButtonPanel";
+import "./ButtonPanel.css";
+import "./Calculator.css"
+
+
+class Calculator extends React.Component {
+    render(){
+
+        return (
+            <div className="component-app">
+                {/* display zone */}
+                <div style={{width : 300}}>
+                    <h3 style={{padding:5}}>000</h3>
+                </div>
+                {/* buttons and signs */}
+                <ButtonPanel />
+            </div>
+        )
+    }
+}
+
+export default Calculator;
